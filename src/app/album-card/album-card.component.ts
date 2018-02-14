bd3a414f1b72ee2dfd7232c9fac599e98c9b8155
Dresoy Ravelo <dresoy@hotmail.com>
2018-02-14 15:07:19 -0300

    - Added border color to the card - Added Min-heigth to the card body

--- src/app/album-card/album-card.component.ts
+++ src/app/album-card/album-card.component.ts
@@ -9,10 +9,40 @@ import { Album } from '../album';
 export class AlbumCardComponent implements OnInit {
 
   @Input() data: Album;
+  @Input() indexValue: number;
+
+  borderClass;
 
   constructor() { }
 
   ngOnInit() {
+    this.borderClass = getBorder(this.indexValue);
   }
 
+
+
+}
+
+function getBorder(index) {
+
+  var borderClass = 'card';
+
+  switch (index) {
+    case 0:
+      borderClass = 'card border-green';
+      break;
+
+    case 1:
+      borderClass = 'card border-blue';
+      break;
+
+    case 2:
+      borderClass = 'card border-purple';
+      break;
+
+    default:
+      break;
+  }
+
+  return borderClass;
 }
